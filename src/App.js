ba2f5ca6a46c3e4f178a7d00237c3347de35cc03
علي يحيي زكريا <alyyehiazak@gmail.com>
2024-12-02 19:14:30 +0200

    added css file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
-    <div>
+    <>
       
-    </div>
+    </>
   );
 }
 
